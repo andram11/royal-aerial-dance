8934f642717cdd504065e4e5c681b07bf5e520d2
andram11 <mertilosandra@gmail.com>
2024-07-22 16:58:34 +0200

    added react big calendar to courses page as default view

--- client/src/components/filter/filter.component.tsx
+++ client/src/components/filter/filter.component.tsx
@@ -6,120 +6,121 @@ import styles from './filter.module.css'
 type SearchCriteriaKey = 'category' | 'location' | 'teacher' | 'dayOfWeek' | 'level';
 
 interface SearchCriteria {
-    category: string;
-    location: string;
-    teacher: string;
-    dayOfWeek: string;
-    level: string;
+  category: string;
+  location: string;
+  teacher: string;
+  dayOfWeek: string;
+  level: string;
 }
 
-const Filter: React.FC = ()=> {
-    const dispatch = useAppDispatch();
-    const [searchCriteria, setSearchCriteria] = useState({
-        category: '',
-        location: '',
-        teacher: '',
-        dayOfWeek: '',
-        level: ''
+const Filter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const [searchCriteria, setSearchCriteria] = useState<SearchCriteria>({
+    category: '',
+    location: '',
+    teacher: '',
+    dayOfWeek: '',
+    level: ''
+  });
+
+  useEffect(() => {
+    dispatch(fetchCoursesWithCriteria(searchCriteria));
+  }, [searchCriteria, dispatch]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    const key = name as SearchCriteriaKey;
+
+    setSearchCriteria(prevCriteria => ({
+      ...prevCriteria,
+      [key]: prevCriteria[key]
+        ? `${prevCriteria[key]},${value}`
+        : value.toLowerCase()
+    }));
+  };
+
+  const handleRemoveItem = (name: SearchCriteriaKey, value: string) => {
+    setSearchCriteria(prevCriteria => {
+      const newValue = prevCriteria[name]
+        .split(',')
+        .filter((v: string) => v !== value)
+        .join(',');
+      return {
+        ...prevCriteria,
+        [name]: newValue
+      };
     });
+  };
 
-    useEffect(() => {
-        dispatch(fetchCoursesWithCriteria(searchCriteria));
-    }, [searchCriteria, dispatch]);
+  const getSelectedItems = (criteria: SearchCriteriaKey) =>
+    searchCriteria[criteria].split(',').filter((item: string) => item);
 
-    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        const key = name as SearchCriteriaKey;
+  const renderSelect = (name: SearchCriteriaKey, options: string[]) => (
+    <div className={styles.selectContainer}>
+      <select name={name} value="" onChange={handleChange}>
+        <option value="">{name.charAt(0).toUpperCase() + name.slice(1)}</option>
+        {options.map(option => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <div className={styles.pillsContainer}>
+        {getSelectedItems(name).map(item => (
+          <div key={item} className={styles.pill}>
+            {item}
+            <button onClick={() => handleRemoveItem(name, item)}>
+              x
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
-        setSearchCriteria(prevCriteria => ({
-            ...prevCriteria,
-            [name]: prevCriteria[key]
-                ? `${prevCriteria[key]},${value}`
-                : value.toLowerCase()
-        }));
-    };
+  return (
+    <div className={styles.filterSidebar}>
+      <div className={styles.filterContainer}>
+        {renderSelect('category', [
+          'Pole Dance',
+          'Aerial Hoop',
+          'Yoga',
+          'Heels dance',
+          'Contemporary dance'
+        ])}
+        {renderSelect('location', [
+          'Gent',
+          'Leuven',
+          'Mons',
+          'Brussels',
+          'Namur'
+        ])}
+        {renderSelect('teacher', [
+          'Emma',
+          'Natalia',
+          'Marlo',
+          'Gaby',
+          'Elena'
+        ])}
+        {renderSelect('dayOfWeek', [
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday'
+        ])}
+        {renderSelect('level', [
+          'Beginner',
+          'Intermediate',
+          'Advanced',
+          'Elite',
+          'All levels'
+        ])}
+      </div>
+    </div>
+  );
+};
 
-    const handleRemoveItem = (name: SearchCriteriaKey, value: string) => {
-        setSearchCriteria(prevCriteria => {
-            const newValue = prevCriteria[name]
-                .split(',')
-                .filter((v: string) => v !== value)
-                .join(',');
-            return {
-                ...prevCriteria,
-                [name]: newValue
-            };
-        });
-    };
-
-    const getSelectedItems = (criteria: SearchCriteriaKey) =>
-        searchCriteria[criteria].split(',').filter((item: string) => item);
-
-    const renderSelect = (name: SearchCriteriaKey, options: string[]) => (
-        <div className={styles.selectContainer}>
-            <select name={name} value="" onChange={handleChange}>
-                <option value="">{name.charAt(0).toUpperCase() + name.slice(1)}</option>
-                {options.map(option => (
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
-            <div className={styles.pillsContainer}>
-                {getSelectedItems(name).map(item => (
-                    <div key={item} className={styles.pill}>
-                        {item}
-                        <button onClick={() => handleRemoveItem(name, item)}>
-                            x
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-
-    return (
-        <div className={styles.filterContainer}>
-            {renderSelect('category', [
-                'Pole Dance',
-                'Aerial Hoop',
-                'Yoga',
-                'Heels dance',
-                'Contemporary dance'
-            ])}
-            {renderSelect('location', [
-                'Gent',
-                'Leuven',
-                'Mons',
-                'Brussels',
-                'Namur'
-            ])}
-            {renderSelect('teacher', [
-                'Emma',
-                'Natalia',
-                'Marlo',
-                'Gaby',
-                'Elena'
-            ])}
-            {renderSelect('dayOfWeek', [
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-                'Sunday'
-            ])}
-            {renderSelect('level', [
-                'Beginner',
-                'Intermediate',
-                'Advanced',
-                'Elite',
-                'All levels'
-            ])}
-        </div>
-    );
-
-}
-
-export default Filter
+export default Filter;
--- client/src/pages/courses/courses.tsx
+++ client/src/pages/courses/courses.tsx
@@ -1,40 +1,118 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setCourses } from '../../state/courses/coursesSlice';
 import { getActiveCourses } from '../../api/api-service';
 import Filter from '../../components/filter/filter.component';
 import { RootState } from '../../state/store';
 
-const CoursesPage: React.FC = () => {
+import CalendarView from '../../components/calendarView/calendarView.component';
+import EventDetails from '../../components/eventDetails/eventDetails.component';
+
+import styles from './courses.module.css'
+
+import { CourseEvent } from '../../types/types';
+
+const parseTimeslot = (
+    startDate: Date,
+    endDate: Date,
+    timeslot: string,
+    recurrenceType?: 'weekly' | 'biMonthly' | 'monthly'
+  ): { start: Date[], end: Date[] } => {
+    const [startTime, endTime] = timeslot.split('-');
+    const [startHour, startMinute] = startTime.split(':').map(Number);
+    const [endHour, endMinute] = endTime.split(':').map(Number);
+  
+    const occurrences = [];
+    let currentStartDate = new Date(startDate);
+  
+    while (currentStartDate <= endDate) {
+      const startOccurrence = new Date(currentStartDate);
+      startOccurrence.setHours(startHour, startMinute);
+  
+      const endOccurrence = new Date(currentStartDate); // Correct endOccurrence calculation
+      endOccurrence.setHours(endHour, endMinute);
+  
+      occurrences.push({ start: startOccurrence, end: endOccurrence });
+  
+      if (recurrenceType === 'weekly') {
+        currentStartDate.setDate(currentStartDate.getDate() + 7);
+      } else if (recurrenceType === 'biMonthly') {
+        currentStartDate.setMonth(currentStartDate.getMonth() + 2);
+      } else if (recurrenceType === 'monthly') {
+        currentStartDate.setMonth(currentStartDate.getMonth() + 1);
+      } else {
+        break;
+      }
+    }
+  
+    return {
+      start: occurrences.map(o => o.start),
+      end: occurrences.map(o => o.end),
+    };
+  };
+  
+  const CoursesPage: React.FC = () => {
     const dispatch = useAppDispatch();
-    const {filteredCourses, loading, error, totalItems, page, pageLimit} = useAppSelector((state: RootState)=> state.courses);
-
+    const { filteredCourses, loading, error } = useAppSelector((state: RootState) => state.courses);
+  
+    const [events, setEvents] = useState<CourseEvent[]>([]);
+    const [selectedEvent, setSelectedEvent] = useState<CourseEvent | null>(null);
+  
     useEffect(() => {
-        const fetchCourses = async () => {
-            const coursesData = await getActiveCourses();
-            dispatch(setCourses(coursesData));
-        };
-
-        fetchCourses();
+      const fetchCourses = async () => {
+        const coursesData = await getActiveCourses();
+        dispatch(setCourses(coursesData));
+      };
+  
+      fetchCourses();
     }, [dispatch]);
-
+  
+    useEffect(() => {
+      const courseEvents = filteredCourses.flatMap((course) => {
+        const recurrenceType = ['weekly', 'biMonthly', 'monthly'].includes(course.recurrenceType) ? course.recurrenceType as 'weekly' | 'biMonthly' | 'monthly' : undefined;
+        const { start, end } = parseTimeslot(new Date(course.startDate), new Date(course.endDate), course.timeslot, recurrenceType);
+  
+        return start.map((startOccurrence, index) => ({
+          id: `${course._id}-${index}`,
+          title: course.title,
+          start: startOccurrence,
+          end: end[index],
+          description: course.teacher,
+          recurrent: course.recurrent,
+          recurrenceType: recurrenceType,
+        }));
+      });
+  
+      setEvents(courseEvents);
+    }, [filteredCourses]);
+  
+    const handleSelectEvent = (event: CourseEvent) => {
+      setSelectedEvent(event);
+    };
+  
+    const handleAddToCart = (courseId: string) => {
+      console.log(`Course ${courseId} added to cart.`);
+    };
+  
     return (
-        <div>
-            <h1>Courses</h1>
-            <Filter />
-            {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            <div>
-                {filteredCourses && filteredCourses.map(course => (
-                    <div key={course._id}>
-                        <h2>{course.title}</h2>
-                        <p>{course.category}</p>
-                    </div>
-                ))}
-            </div>
+      <div className={styles.coursesPage}>
+        <Filter />
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        <div className={styles.calendarContainer}>
+          <h2>Courses</h2>
+          <CalendarView events={events} onSelectEvent={handleSelectEvent} />
+          {selectedEvent && (
+            <EventDetails
+              event={selectedEvent}
+              onAddToCart={handleAddToCart}
+              onClose={() => setSelectedEvent(null)}
+            />
+          )}
         </div>
+      </div>
     );
-};
-
-export default CoursesPage;
+  };
+  
+  export default CoursesPage;
