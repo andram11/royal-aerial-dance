95d5f793c9a74721056bb8a260bfd871b6e6fb06
andram11 <mertilosandra@gmail.com>
2024-08-23 19:02:13 +0200

    Updates to vite config for deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 import { configDefaults } from 'vitest/config';
+
+// Determine if we're in the development environment
+const isDev = process.env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'server.cert')),
-    },
+    ...(isDev && {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'server.cert')),
+      },
+    }),
   },
   test: {
     globals: true,
     environment: 'jsdom', // You’ll need jsdom for testing React components
     setupFiles: './setupTests.ts', // Path to setup file (optional)
   },
-})
+});
