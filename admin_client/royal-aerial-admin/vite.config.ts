8ebea855e4b11f5be2d0148e185e8a71b9fbe37b
andram11 <mertilosandra@gmail.com>
2024-09-29 11:12:04 +0200

    Fixes for vite config

--- admin_client/royal-aerial-admin/vite.config.ts
+++ admin_client/royal-aerial-admin/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
